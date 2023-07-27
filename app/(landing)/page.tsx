42ad38e541e8dba4fd3dfe3f57a7d1e288f0947d
saymanrabbi <mdrabbi.asm@gmail.com>
2023-07-27 18:07:54 +0600

    login user with cool clerk package

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,5 +1,22 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function LandingPage() {
   return (
+    <div>
    <p>This page view all user</p>
+   <div>
+    <Link  href='/sign-in'>
+   <Button>
+    Login
+   </Button>
+   </Link>
+   <Link  href='/sign-up'>
+   <Button>
+    Register
+   </Button>
+   </Link>
+   </div>
+   </div>
   )
 }
