a726ef3bc4264909bccb6bb644a8381410890aae
saymanrabbi <mdrabbi.asm@gmail.com>
2023-07-29 17:27:52 +0600

    image genaration

--- app/(dashboard)/(routes)/image/constants.ts
+++ app/(dashboard)/(routes)/image/constants.ts
@@ -1,45 +1,47 @@
-import * as z from 'zod';
+import * as z from "zod";
+
 export const formSchema = z.object({
-    prompt:z.string().min(1,{
-        message:'Image Prompt is required'
-    }),
-    amount:z.string().min(1),
-    resulation:z.string().min(1),
-})
+  prompt: z.string().min(1, {
+    message: "Photo prompt is required"
+  }),
+  amount: z.string().min(1),
+  resolution: z.string().min(1),
+});
 
 export const amountOptions = [
-    {
-        value:'1',
-        label:'1 Photo'
-    },
-    {
-        value:'2',
-        label:'2 Photos'
-    },
-    {
-        value:'3',
-        label:'3 Photos'
-    },
-    {
-        value:'4',
-        label:'4 Photos'
-    },
-    {
-        value:'5',
-        label:'5 Photos'
-    }
-]
+  {
+    value: "1",
+    label: "1 Photo"
+  },
+  {
+    value: "2",
+    label: "2 Photos"
+  },
+  {
+    value: "3",
+    label: "3 Photos"
+  },
+  {
+    value: "4",
+    label: "4 Photos"
+  },
+  {
+    value: "5",
+    label: "5 Photos"
+  }
+];
 
-export const resulationOptions = [
-    {
-        value:'256 x 256',
-        label:'256 x 256'
-    },
-    {
-        value:'512 x 512',
-        label:'512 x 512'
-    },{
-        value:'1024 x 1024',
-        label:'1024 x 1024'
-    }
-]
+export const resolutionOptions = [
+  {
+    value: "256x256",
+    label: "256x256",
+  },
+  {
+    value: "512x512",
+    label: "512x512",
+  },
+  {
+    value: "1024x1024",
+    label: "1024x1024",
+  },
+];
